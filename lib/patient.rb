d4ab1d20572e9662ddf6033819ea38598b55d3ef
Sierra Wallace <sskwallace@gmail.com>
2020-10-03 23:52:34 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,24 @@
+class Patient 
+  attr_accessor :name
+  @@all = [] 
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  def self.all 
+    @@all 
+  end
+  def new_appointment(date, doctor)
+    Appointment.new(date, doctor, self)
+  end
+  def appointments 
+    Appointment.all.select do |appt| 
+      appt.doctor
+    end
+  end
+  def doctors 
+    appointments.map do |appt| 
+      appt.patient
+  end
+end
+end
