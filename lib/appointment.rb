fd42d9e1311a6cd733911ace92de244c87f08132
Sierra Wallace <sskwallace@gmail.com>
2020-10-04 00:09:43 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,7 +3,7 @@ class Appointment
   attr_accessor :date, :patient, :doctor
   @@all = [] 
   def initialize(date, patient, doctor)
-    #binding.pry
+    binding.pry
     @date = date
     @patient = patient
     @doctor = doctor
